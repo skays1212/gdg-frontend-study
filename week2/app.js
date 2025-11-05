bba3a094a21abd065984510022dd1dba2c87730e
skays12 <juju453@naver.com>
2025-11-05 17:20:31 +0900

    week3

---
+++ week2/app.js
@@ -0,0 +1,20 @@
+const number = document.getElementById("counting-num");
+
+const incButton = document.getElementById("increase");
+
+
+let count = 0;
+
+number.textContent = count;
+
+
+function increaseCount() {
+
+ count++; // count = count + 1
+
+ number.textContent = count; // 숫자 변화 반영
+
+}
+
+
+incButton.addEventListener("click", increaseCount);
